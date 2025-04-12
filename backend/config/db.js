9e5f3f01d4f7997a93a65806c672907bd110f91d
SOURYA DEY <142716790+sourya001@users.noreply.github.com>
2025-04-12 10:45:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://2023ugee048:JVJebDmwY2FFQaUj@cluster0.2bqmly3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://2023ugee048:JVJebDmwY2FFQaUj@cluster0.2bqmly3.mongodb.net/project 0').then(()=>console.log("DB Connected"));
 }
