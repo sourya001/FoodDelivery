9abcaaa45e010382b00294218983cbe51c1274ef
SOURYA DEY <142716790+sourya001@users.noreply.github.com>
2025-04-12 10:46:39 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://2023ugee048:JVJebDmwY2FFQaUj@cluster0.2bqmly3.mongodb.net/project 0').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://2023ugee048:JVJebDmwY2FFQaUj@cluster0.2bqmly3.mongodb.net/Project 0').then(()=>console.log("DB Connected"));
 }
